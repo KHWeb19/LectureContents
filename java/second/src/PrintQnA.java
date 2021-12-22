87e6dcf874d9c52847aa5043680e7b8a487b0bce
silenc3502 <gcccompil3r@gmail.com>
2021-12-22 19:31:46 +0900

    엔터가 있고 없고 차이

---
+++ java/second/src/PrintQnA.java
@@ -0,0 +1,14 @@
+public class PrintQnA {
+    public static void main(String[] args) {
+        int num1 = 3, num2 = 7, num3 = 33;
+
+        // 엔터 없으면 다음이 붙어 나옴
+        System.out.print("print 엔터 없음");
+        System.out.println("println 엔터 적용");
+        System.out.printf("printf 엔터 없음");
+
+        System.out.print("print 요렇게 엔터\n");
+        System.out.println("println 쓰면 엔터");
+        System.out.printf("printf는 요래 쓰면 엔터\n");
+    }
+}
