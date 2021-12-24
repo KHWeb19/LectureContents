5bd56e3b3ee34b8f66ed15107da5cfd0cc2b955a
silenc3502 <gcccompil3r@gmail.com>
2021-12-24 16:11:47 +0900

    1번 문제 답

--- java/fourth/src/Answer1.java
+++ java/fourth/src/Answer1.java
@@ -6,6 +6,8 @@ public class Answer1 {
         int range = MAX - MIN + 1;
         boolean isChar = true;
 
+        // 루프는 보편적으로 반복 작업하는 녀석들을 루프라고 함
+        // while, for 요런애들이 루프(loop)
         while (isChar) {
             int rand = (int) (Math.random() * range + MIN);
 
@@ -14,10 +16,11 @@ public class Answer1 {
             boolean condition1 = rand >= 65 && rand <= 90;
             boolean condition2 = rand >= 97 && rand <= 122;
 
+            // if()는 루프가 아님
             if (condition1 || condition2) {
                 System.out.printf("rand는 영문자 대소문자중 하나임: %c(%d)\n", rand, rand);
                 isChar = false;
-                // break를 걸면 제어문 while, for 등을 바로 빠져나감
+                // break를 걸면 loop(반복)을 바로 빠져나감
                 break;
             }
 
