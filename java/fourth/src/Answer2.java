acf6b593b004d49c7482e80520b5456a088c9f19
silenc3502 <gcccompil3r@gmail.com>
2021-12-24 18:19:37 +0900

    2번 문제 final로 개선시킴

--- java/fourth/src/Answer2.java
+++ java/fourth/src/Answer2.java
@@ -5,11 +5,12 @@ public class Answer2 {
         int result = 0, i;
 
         final int START = 2;
+        final int END = 20;
 
         // 컴퓨터에서 배열의 시작이 0이기 때문에
         // 반복 시작을 0을 기준으로 맞춰주는 것이 좋습니다.
         // 그래서 START 2는 결국 3번째
-        for (i = START; i < 20; i++) {
+        for (i = START; i < END; i++) {
             result = first + second;
             first = second;
             second = result;
