7f5e0023b73a3f520205e9f9744c9742d9ce44a8
silenc3502 <gcccompil3r@gmail.com>
2021-12-29 18:11:21 +0900

    import를 하는 케이스 vs 안하는 케이스

---
+++ java/7th/src/QnA5.java
@@ -0,0 +1,11 @@
+public class QnA5 {
+    public static void main(String[] args) {
+        /* Q. 배열 생성했는데 왜 맨 위에 import를 지정안해도 오류가 안나는건가요? */
+
+        /* A. Scanner는 사실 class(클래스)에 해당합니다.
+              외부에 있는 class를 땡겨와서 사용할 땐 반드시 import를 사용해서
+              요러한 class를 사용하겠다라고 명시를 해줘야 합니다.
+              그렇기 때문에 Scanner는 import를 사용한 반면
+              배열은 Java 자체에 내장되어 있는 문법으로 import가 필요 없습니다. */
+    }
+}
