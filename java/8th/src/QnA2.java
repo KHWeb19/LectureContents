354b9f4ae92a3084b1bf38a63d0c7d94f551d4d1
silenc3502 <gcccompil3r@gmail.com>
2021-12-30 16:35:32 +0900

    BigInteger.valueOf()를 통해 어떤 숫자던 BigInteger 형태로 변환 가능

--- java/8th/src/QnA2.java
+++ java/8th/src/QnA2.java
@@ -1,2 +1,22 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class QnA2 {
+    public static void main(String[] args) {
+        final int START = 0;
+
+        BigInteger[] arr;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("몇번째 배수 ? ");
+        int end = sc.nextInt();
+
+        arr = new BigInteger[end];
+
+        for (int i = START; i < end; i++){
+            arr[i] = BigInteger.valueOf((int)(Math.pow(2,i)));
+
+            System.out.println("arr[" +i+ "] = "+arr[i]);
+        }
+    }
 }
