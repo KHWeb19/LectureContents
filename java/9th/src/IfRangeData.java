97bdc0a7164c1da7f3a1ab608a2c36642d163cc8
silenc3502 <gcccompil3r@gmail.com>
2021-12-31 16:01:45 +0900

    if를 활용한 범주형 데이터 다루기(그리고 주의할점)

--- java/9th/src/IfRangeData.java
+++ java/9th/src/IfRangeData.java
@@ -17,11 +17,15 @@ public class IfRangeData {
                 break;
             }
 
+            // 이런 혼선을 최소화하고자 한다면
+            // 가장 협소한 범주를 가장 위에 배치한다.
+            // 가장 넓은 범주는 최하위에 배치한다.
+            // (가장 넓은 범주가 막말로 개나소나 다 할테니까)
             if (num >= 90) {
                 System.out.println("A 등급");
-            } else if (num >= 80) {
+            } else if (num >= 80) {     // 90 > num >= 80
                 System.out.println("B 등급");
-            } else if (num >= 70) {
+            } else if (num >= 70) {     // (90 > num | 80 > num) >= 80
                 System.out.println("C 등급");
             } else if (num >= 60) {
                 System.out.println("D 등급");
