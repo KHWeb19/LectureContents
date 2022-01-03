40b60655a48252c58e611ddc6996778a837b98c4
silenc3502 <gcccompil3r@gmail.com>
2022-01-03 18:13:59 +0900

    문제은행 [4] - 5번 풀이

---
+++ java/10th/src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    String name;
+    int age;
+
+    String major;
+
+    public void initStudents (String name, int age, String major) {
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", major='" + major + '\'' +
+                '}';
+    }
+}
