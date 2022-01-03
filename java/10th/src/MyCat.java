82759cafafa8afbc0f952c52693ceb74b39df29f
silenc3502 <gcccompil3r@gmail.com>
2022-01-03 17:55:26 +0900

    문제은행 [4] - 3번 고양이 키우기 시뮬레이션 풀이 (개선 사항 - 지저분한 부분 없애기)

--- java/10th/src/MyCat.java
+++ java/10th/src/MyCat.java
@@ -9,13 +9,15 @@ public class MyCat {
 
     final int SCHEDULE_NUM = 3;
 
-    int[] scheduleNum;
+    int scheduleNum;
+
+    int simulationCnt;
 
     public void initMyCat (int age, float weight) {
         this.age = age;
         this.weight = weight;
 
-        scheduleNum = new int[SCHEDULE_NUM];
+        simulationCnt = 0;
     }
     private void feed () {
         System.out.println("고오급 사료를 먹습니다!");
@@ -28,9 +30,11 @@ public class MyCat {
         System.out.println(defaultSound);
 
         if (weight > 8) {
-            System.out.printf("운동 안하면 고양이 비만 됩니다: %.1f", weight);
+            System.out.printf("운동 안하면 고양이 비만 됩니다: %.1f\n", weight);
         } else if (weight < 6) {
-            System.out.printf("너무 많이 빼도 위험합니다: %.1f", weight);
+            System.out.printf("너무 많이 빼도 위험합니다: %.1f\n", weight);
+        } else {
+            System.out.printf("현재 고양이는 정상입니다: %.1f\n", weight);
         }
     }
     private void training () {
@@ -50,9 +54,9 @@ public class MyCat {
             System.out.println("오늘은 무엇을 할까요 ? (하루 일과는 3가지를 선택합니다)");
 
             for (int i = 0; i < SCHEDULE_NUM; i++) {
-                scheduleNum[i] = getRandomPattern();
+                scheduleNum = getRandomPattern();
 
-                switch (scheduleNum[i]) {
+                switch (scheduleNum) {
                     case FEED:
                         feed();
                         break;
@@ -81,6 +85,50 @@ public class MyCat {
 
             if (endFlag) {
                 break;
+            }
+        }
+    }
+
+    private boolean checkSimulation () {
+        boolean checkSim = false;
+
+        simulationCnt++;
+        System.out.printf("%d 일차!", simulationCnt);
+
+        if (simulationCnt > 365) {
+            System.out.println("냥이는 가족들과 함께 오래오래 햄볶하게 살았답니다 ^^");
+        } else if (weight > 12.0f) {
+            System.out.println("병원 ㅠ");
+        } else if (weight < 5.1f) {
+            System.out.println("병원 가요 ㅠ");
+        } else {
+            checkSim = true;
+            return checkSim;
+        }
+
+        return checkSim;
+    }
+
+    public void simulation2 () {
+        boolean endFlag = false;
+        int cnt = 0;
+
+        while (checkSimulation()) {
+            System.out.println("오늘은 무엇을 할까요 ? (하루 일과는 3가지를 선택합니다)");
+
+            for (int i = 0; i < SCHEDULE_NUM; i++) {
+                scheduleNum = getRandomPattern();
+
+                switch (scheduleNum) {
+                    case FEED:
+                        feed();
+                        break;
+                    case TRAINING:
+                        training();
+                        break;
+                }
+
+                warningMessage();
             }
         }
     }
