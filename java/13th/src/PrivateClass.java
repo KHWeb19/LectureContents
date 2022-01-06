4c4605492258aac02b3658f635cd0471b303ae4e
silenc3502 <gcccompil3r@gmail.com>
2022-01-06 20:20:42 +0900

    private이 필요한 이유와 private 상에서 데이터 처리

--- java/13th/src/PrivateClass.java
+++ java/13th/src/PrivateClass.java
@@ -1,2 +1,17 @@
 public class PrivateClass {
+    private int age;
+    private String name;
+
+    public void setPersonInfo (int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "NonPrivateClass{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- java/13th/src/PrivateClassExample.java
+++ java/13th/src/PrivateClassExample.java
@@ -1,2 +1,14 @@
 public class PrivateClassExample {
+    public static void main(String[] args) {
+        PrivateClass pc = new PrivateClass();
+
+        /* 이 코드는 클래스 내 변수가 private이므로 컴파일 에러!
+        pc.age = 20;
+        pc.name = "왜 안될까 ?"; */
+
+        // 그러므로 우리는 매서드나 생성자를 통해서 값을 설정했다.
+        pc.setPersonInfo(20, "이렇게 해야 되는구나 ?");
+
+        System.out.println(pc);
+    }
 }
