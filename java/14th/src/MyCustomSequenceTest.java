dc7b547b09372b7c3dfb4da66584da55899094d3
silenc3502 <gcccompil3r@gmail.com>
2022-01-07 16:28:02 +0900

    문제은행 [3] - 1번 클래스화 + 문제은행 [2] 재활용 + BigInteger 처리

--- java/14th/src/MyCustomSequenceTest.java
+++ java/14th/src/MyCustomSequenceTest.java
@@ -1,10 +1,12 @@
 public class MyCustomSequenceTest {
     public static void main(String[] args) {
         final int ORDER = 31;
+        final int ORDER2 = 50;
 
         MyCustomSequence mcs = new MyCustomSequence(2);
 
         // 2^0 <<<
         System.out.printf("2^%d = %d\n", ORDER - 1, mcs.getGeoSeqNthOrderData(ORDER));
+        System.out.printf("2^%d = %d\n", ORDER2 - 1, mcs.getGeoSeqNthOrderData(ORDER2));
     }
 }
--- java/14th/src/NonInitialSet.java
+++ java/14th/src/NonInitialSet.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 public class NonInitialSet {
     int equalRatio;
 
@@ -5,7 +7,13 @@ public class NonInitialSet {
         this.equalRatio = equalRatio;
     }
 
-    public int getGeoSeqNthOrderData (final int count) {
-        return (int) (Math.pow(equalRatio, count - 1));
+    public BigInteger getGeoSeqNthOrderData (final int count) {
+        // 디버깅용(값이 올바르게 나오나 체크)
+        for (int i = 0; i < count; i++) {
+            System.out.printf("%d^%d = %d\n", equalRatio, i,
+                    BigInteger.valueOf(equalRatio).pow(i));
+        }
+
+        return BigInteger.valueOf(equalRatio).pow(count - 1);
     }
 }
