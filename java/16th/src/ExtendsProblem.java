ad6665e6be7d23525f7fdef7ac3a428f9646e8c3
silenc3502 <gcccompil3r@gmail.com>
2022-01-11 16:51:36 +0900

    상속, 인터페이스 응용 문제 풀어보기

---
+++ java/16th/src/ExtendsProblem.java
@@ -0,0 +1,5 @@
+public class ExtendsProblem {
+    public static void main(String[] args) {
+        // 주사위 게임 만들었던 내용에 상속을 도입해서 변경해봅시다!
+    }
+}
---
+++ java/16th/src/InterfaceProblem.java
@@ -0,0 +1,10 @@
+public class InterfaceProblem {
+    public static void main(String[] args) {
+        // 앞서 이야기 했듯이 게임을 하나 만들어봅시다.
+        // qSkill, wSkill 두 개 정도를 만들고
+        // 캐릭터가 많으면 힘드니까 전사, 마법사 정도로 처리하면 되겠습니다.
+        // 여기서도 모험가 -> 전사 -> 2차 전직(전사)
+        //         모험가 -> 법사 -> 2차 전직(법사)
+        // 이와 같은 형식으로 설정도 가능할 것입니다.
+    }
+}
