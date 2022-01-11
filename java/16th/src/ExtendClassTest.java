9e7578576c69cc8417ef61de8d1b474ddccb8663
silenc3502 <gcccompil3r@gmail.com>
2022-01-11 15:51:26 +0900

    자바 상속 기본 예제

---
+++ java/16th/src/ExtendClassTest.java
@@ -0,0 +1,7 @@
+public class ExtendClassTest {
+    public static void main(String[] args) {
+        Student student = new Student("안녕", 20, "전기/전자");
+
+        System.out.println(student);
+    }
+}
