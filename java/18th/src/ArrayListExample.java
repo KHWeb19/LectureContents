ea3a9f0dc4a678f13e6ea89957765effc86df3f5
silenc3502 <gcccompil3r@gmail.com>
2022-01-13 18:15:19 +0900

    ArrayList 기본 예제

--- java/18th/src/ArrayListExample.java
+++ java/18th/src/ArrayListExample.java
@@ -8,14 +8,21 @@ public class ArrayListExample {
         // 데이터 타입 <<<<---- 을 넣어도 된다고 했으니 어떤 것도 들어갈 수 있다 ?
         ArrayList<String> list = new ArrayList<>();
 
+        // add(x): 그냥 현재 위치에서 순차적으로 값이 입력됨
+        //        현재 위치란 처음엔 0, 그다음엔 1, 그 다음은 2
+        //        이와 같이 순차적인 구조를 의미함
         list.add("우유");
         list.add("계란");
         list.add("버터");
+        // add(x, y): x 인덱스에 y값을 넣는 형식임
+        //            즉 특정한 위치에 값을 넣을수가 있음
         list.add(1, "사과");
         list.add(2, "파인애플");
         list.add(4, "포도");
-
+        
+        // size(): ArrayList의 길이를 알려줌
         for (int i = 0; i < list.size(); i++) {
+            // get(x): x 인덱스의 정보를 출력함
             System.out.println(list.get(i));
         }
 
