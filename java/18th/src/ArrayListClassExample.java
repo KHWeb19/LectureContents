94ff8638c0289f05ed0074cb854d955ea79c9a26
silenc3502 <gcccompil3r@gmail.com>
2022-01-13 19:15:58 +0900

    ArrayList와 클래스의 연동

--- java/18th/src/ArrayListClassExample.java
+++ java/18th/src/ArrayListClassExample.java
@@ -4,18 +4,20 @@ public class ArrayListClassExample {
     public static void main(String[] args) {
         final int ACCOUNT_NUM = 3;
 
+        // Adventurer 객체를 저장할 수 있는 ArrayList를 생성
         ArrayList<Adventurer> advList = new ArrayList<>();
 
         Adventurer[] advArr = new Adventurer[ACCOUNT_NUM];
 
         for (int i = 0; i < ACCOUNT_NUM; i++) {
             advArr[i] = new Adventurer();
+            // 실제 ArrayList에 생성한 객체를 저장함
             advList.add(advArr[i]);
         }
 
         for (int i = 0; i < ACCOUNT_NUM; i++) {
             System.out.println("계정 정보를 출력합니다!");
-
+            // 그리고 get을 통해 객체 정보를 얻어오는 것 또한 잘 됨
             System.out.println(advList.get(i));
         }
     }
