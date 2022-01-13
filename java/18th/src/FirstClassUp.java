b369cf2a15f9d97a9a1df7ea5307f3fc959c8b1a
silenc3502 <gcccompil3r@gmail.com>
2022-01-13 17:39:51 +0900

    protected의 유용성

---
+++ java/18th/src/FirstClassUp.java
@@ -0,0 +1,9 @@
+public class FirstClassUp extends Adventurer {
+    public FirstClassUp () {
+        super();
+    }
+
+    public void iCanControlSuperContents () {
+        System.out.printf("Wow - str: %d, con: %d\n", str, con);
+    }
+}
---
+++ java/18th/src/ProtectedTest.java
@@ -0,0 +1,7 @@
+public class ProtectedTest {
+    public static void main(String[] args) {
+        FirstClassUp fcu = new FirstClassUp();
+
+        fcu.iCanControlSuperContents();
+    }
+}
