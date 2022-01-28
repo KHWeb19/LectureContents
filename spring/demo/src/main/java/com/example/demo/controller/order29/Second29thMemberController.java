bcf4b50de40b23f2493b643c8c88ef402ffd6c31
silenc3502 <gcccompil3r@gmail.com>
2022-01-28 17:40:15 +0900

    주석 일부 추가

--- spring/demo/src/main/java/com/example/demo/controller/order29/Second29thMemberController.java
+++ spring/demo/src/main/java/com/example/demo/controller/order29/Second29thMemberController.java
@@ -12,12 +12,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/29th/member")
 public class Second29thMemberController {
 
+    // 실제로는 아래 코드에서 리턴되는 member는 그냥 객체(메모리 주소)일 뿐인데
+    // 웹 상에서 출력되는 정보는 JSON 형태로 출력되었다.
+    // 이를 지원하는 녀석은 ResponseBody에 해당한다.
     @ResponseBody
     @GetMapping("/view-info")
     public Member viewInfo () {
         log.info("viewInfo()");
 
         Member member = new Member();
+        log.info("member: " + member);
 
         return member;
     }
