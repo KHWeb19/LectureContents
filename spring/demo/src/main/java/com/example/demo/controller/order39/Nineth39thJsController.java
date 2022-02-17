b5b7243a09740bb33877945be8a9468af57dfd15
silenc3502 <gcccompil3r@gmail.com>
2022-02-17 16:11:15 +0900

    참/거짓 판정

---
+++ spring/demo/src/main/java/com/example/demo/controller/order39/Nineth39thJsController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller.order39;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/39th/js")
+public class Nineth39thJsController {
+
+    // 프로토타입 (prototype) 이란 ?
+    // 리턴 타입이 무엇이고
+    // 매서드(함수) 이름은 무엇이고
+    // 인자로 취하는 것은 무엇인지를 전반적으로 나타냄
+    // 그래서 프로토타입을 보고 뭐하는 것이다라는 전반적 유추가 가능해야함
+    @GetMapping("/jsBoolTest")
+    public String jsBoolTest () {
+        log.info("jsBoolTest()");
+
+        return "/39th/js/jsBoolTest";
+    }
+}
---
+++ spring/demo/src/main/resources/static/js/39th/boolTest.js
@@ -0,0 +1,43 @@
+function BoolTest () {
+    let emptyObj = {}
+
+    let add = function (a, b) {
+        return a + b
+    }
+
+    function Vehicle () {
+        this.vendor = "Volvo"
+        this.mcu = "tms570"
+        this.price = 200000000
+    }
+
+    Vehicle.prototype.getVendor = function () {
+        return this.vendor
+    }
+
+    let vehicle = new Vehicle();
+
+    console.log("객체(emptyObj): " + emptyObj)
+
+    if (emptyObj) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
+
+    console.log("객체(add): " + add)
+
+    if (add) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
+
+    console.log("객체(vehicle.vendor): " + vehicle.vendor)
+
+    if (vehicle.vendor) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
+}
