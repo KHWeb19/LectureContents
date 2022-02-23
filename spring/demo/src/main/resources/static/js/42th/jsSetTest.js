cf825aeed907549b8b06d0080a6467a1d0d76ae3
silenc3502 <gcccompil3r@gmail.com>
2022-02-23 19:31:05 +0900

    js Set 완료

--- spring/demo/src/main/resources/static/js/42th/jsSetTest.js
+++ spring/demo/src/main/resources/static/js/42th/jsSetTest.js
@@ -15,6 +15,22 @@ function JsSetTest () {
     console.log(set2.has("Grape"))
     console.log(set2.has("Strawberry"))
 
-    set2.clear()
-    console.log(set2.has("Grape"))
+    //set2.clear()
+    //console.log(set2.has("Grape"))
+
+    let setKeyIter = set2.keys()
+
+    for (let key of setKeyIter) {
+        console.log("key: " + key)
+    }
+
+    let setEntryIter = set2.entries()
+
+    for (let entry of setEntryIter) {
+        console.log("entry: " + entry)
+    }
+
+    set2.forEach(function(val1, val2) {
+        console.log(val1 + " : " + val2)
+    })
 }
