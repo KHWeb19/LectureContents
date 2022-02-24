aaf2acbe3e41cd4b9e490ba81a86a0dfd66bcaf7
silenc3502 <gcccompil3r@gmail.com>
2022-02-24 18:04:36 +0900

    병렬처리 3

---
+++ spring/demo/src/main/resources/static/js/43th/promiseParallelThree.js
@@ -0,0 +1,54 @@
+function plus (num1, num2) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            res = num1 + num2
+            console.log(num1 + " + " + num2 + " = ")
+            resolve(res)
+        }, 2000)
+    })
+}
+
+function minus (num1, num2) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            res = num1 - num2
+            console.log(num1 + " - " + num2 + " = ")
+            resolve(res)
+        }, 1500)
+    })
+}
+
+function divide (num1, num2) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            res = num1 / num2
+            console.log(num1 + " / " + num2 + " = ")
+            resolve(res)
+        }, 1000)
+    })
+}
+
+function multiply (num1, num2) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            res = num1 * num2
+            console.log(num1 + " * " + num2 + " = ")
+            resolve(res)
+        }, 500)
+    })
+}
+
+function PromiseParallelThree () {
+    console.log("JavaScript Promise Parallel Test")
+
+    Promise.race([
+        plus(100, 200),
+        minus(100, 200),
+        multiply(100, 200),
+        divide(100, 200)
+    ]).then(function (res) {
+        console.log(res)
+    })
+
+    console.log("자바 스크립트는 스레드 기반(추상화되어 있을뿐)입니다.")
+}
