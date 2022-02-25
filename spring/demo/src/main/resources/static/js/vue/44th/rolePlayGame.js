010de4d130cea86dfdec929cce7d58f16843b342
silenc3502 <gcccompil3r@gmail.com>
2022-02-25 17:39:08 +0900

    vue로 rpg 게임 만들기 ?

--- spring/demo/src/main/resources/static/js/vue/44th/rolePlayGame.js
+++ spring/demo/src/main/resources/static/js/vue/44th/rolePlayGame.js
@@ -6,6 +6,20 @@ let app = new Vue({
             { id: 2, name: '고블린', hp: 200 },
             { id: 3, name: '사슴', hp: 150 },
             { id: 4, name: '리본돼지', hp: 250}
-        ]
+        ],
+        name: '일단은 고정값: 키메라다!!!'
+    },
+    methods: {
+        addMonster: function () {
+            let max = this.list.reduce(function(a, b) {
+                return a > b.id ? a : b.id
+            }, 0)
+
+            this.list.push({
+                id: max + 1,
+                name: this.name,
+                hp: 500
+            })
+        }
     }
 })
