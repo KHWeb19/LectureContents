4ae923b6ddad17a08f0bbf968465e86e131d9a4a
silenc3502 <gcccompil3r@gmail.com>
2022-03-04 17:52:05 +0900

    48: vue 전역 컴포넌트 등록하기

---
+++ vcli/frontend_lecture/src/components/test/GlobalComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="addCounter">{{ counter }}</button>
+</template>
+
+<script>
+
+export default {
+    name: 'global-component',
+    props: ['initialTest'],
+    data () {
+        return {
+            counter: this.initialTest
+        }
+    },
+    methods: {
+        addCounter () {
+            this.counter += 1
+        }
+    }
+}
+
+</script>
