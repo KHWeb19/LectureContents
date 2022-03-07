19ee5c70940189b70ea1842eea988cd11dd5f1ec
silenc3502 <gcccompil3r@gmail.com>
2022-03-07 19:37:57 +0900

    Vue + Spring 게시물 등록

--- spring/demo/src/main/java/com/example/demo/controller/vue/order48/Eighteenth48thVueBoardController.java
+++ spring/demo/src/main/java/com/example/demo/controller/vue/order48/Eighteenth48thVueBoardController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,14 @@ public class Eighteenth48thVueBoardController {
         log.info("getVueBoardList()");
 
         return new ResponseEntity<>(service.list(), HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Board> postRegisterBoard (@RequestBody Board board) {
+        log.info("postRegisterBoard()");
+
+        service.register(board);
+
+        return new ResponseEntity<>(board, HttpStatus.OK);
     }
 }
