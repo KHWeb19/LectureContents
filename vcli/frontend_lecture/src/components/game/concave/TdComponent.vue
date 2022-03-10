d519c43329bdbe1fdda8a7a5ed807915408eaf1f
silenc3502 <gcccompil3r@gmail.com>
2022-03-10 18:07:28 +0900

    3 by 3 board & use emit

--- vcli/frontend_lecture/src/components/game/concave/TdComponent.vue
+++ vcli/frontend_lecture/src/components/game/concave/TdComponent.vue
@@ -9,7 +9,10 @@
 export default {
     data () {
         return {
-            
+            game: {
+                propTurn: this.turn,
+                propWin: this.winner
+            }
         }
     },
     props: {
@@ -22,7 +25,60 @@ export default {
     },
     methods: {
         onClickTd () {
-            alert('마우스 클릭 감지!')
+            if (this.cellData) {
+                return
+            }
+
+            this.$set(this.tableData[this.rowIndex], this.cellIndex, this.turn)
+
+            let win = false
+
+            if (
+                this.tableData[this.rowIndex][0] === this.turn &&
+                this.tableData[this.rowIndex][1] === this.turn &&
+                this.tableData[this.rowIndex][2] === this.turn
+            ) {
+                win = true
+            } else if (
+                this.tableData[0][this.cellIndex] === this.turn &&
+                this.tableData[1][this.cellIndex] === this.turn &&
+                this.tableData[2][this.cellIndex] === this.turn
+            ) {
+                win = true
+            } else if (
+                this.tableData[0][0] === this.turn &&
+                this.tableData[1][1] === this.turn &&
+                this.tableData[2][2] === this.turn
+            ) {
+                win = true
+            } else if (
+                this.tableData[0][2] === this.turn &&
+                this.tableData[1][1] === this.turn &&
+                this.tableData[2][0] === this.turn
+            ) {
+                win = true
+            }
+
+            if (win) {
+                this.game.propWin = this.turn
+            } else {
+                let all = true
+                this.tableData.forEach(row => {
+                    row.forEach(cell => {
+                        if (!cell) {
+                            all = false
+                        }
+                    })
+                })
+
+                if (all) {
+                    this.propWin = ''
+                } else {
+                    this.game.propTurn = this.turn === 'O' ? 'X' : 'O'
+
+                    this.$emit('updateTurn', this.game.propTurn)
+                }
+            }
         }
     }
 }
