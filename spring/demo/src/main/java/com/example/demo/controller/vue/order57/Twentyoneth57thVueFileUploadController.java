7a9205fd86ce160574f4a8fd081b451bc46efe47
silenc3502 <gcccompil3r@gmail.com>
2022-03-18 17:46:58 +0900

    57: Success to upload Image

--- spring/demo/src/main/java/com/example/demo/controller/vue/order57/Twentyoneth57thVueFileUploadController.java
+++ spring/demo/src/main/java/com/example/demo/controller/vue/order57/Twentyoneth57thVueFileUploadController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileOutputStream;
 import java.util.List;
 
 @Slf4j
@@ -23,6 +24,17 @@ public class Twentyoneth57thVueFileUploadController {
             for (MultipartFile multipartFile : fileList) {
                 log.info("requestUploadFile() - Make file: " +
                         multipartFile.getOriginalFilename());
+
+                // 필요하다면 생성 날자와 시간(date) 정보를 추가
+                // 좀 더 구별이 필요하다면 아이디까지 같이 결합시켜도 무방
+                // 아이디/날자/이미지
+                FileOutputStream writer = new FileOutputStream(
+                        "./images/" + multipartFile.getOriginalFilename());
+
+                log.info("디렉토리에 파일 배치 성공!");
+
+                writer.write(multipartFile.getBytes());
+                writer.close();
             }
         } catch (Exception e) {
             return "Upload Fail!!!";
