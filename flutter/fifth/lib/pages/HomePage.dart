c16592b0025510d71cb6b9cf60ef51ac04c362f0
silenc3502 <gcccompil3r@gmail.com>
2022-06-09 17:40:51 +0900

    다섯번째

--- flutter/fifth/lib/pages/HomePage.dart
+++ flutter/fifth/lib/pages/HomePage.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../components/Logo.dart';
+import '../utility/size.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.lightBlue,
-      ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              SizedBox(height: xxl_gap),
+              Logo("에디로봇아카데미"),
+              SizedBox(height: xm_gap),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("시작!"),
+              )
+            ],
+          ),
+        )
     );
   }
 }
